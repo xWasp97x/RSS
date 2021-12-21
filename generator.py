8f86b7d6d8ff34e41b9294039b011311f326ecfa
wasp97 <imirc97@gmail.com>
2021-12-21 18:30:49 +0100

    Added some logging messages

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ from rfeed import *
 import wget
 from time import sleep
 import os
+from loguru import logger
 
 PORT = 8080
 
@@ -33,6 +34,7 @@ def generate_rss():
 
     rss = feed.rss()
 
+    logger.info(f'Writing  {output_file}')
     with open(output_file, 'w') as file:
         file.write(rss)
 
@@ -42,5 +44,7 @@ if __name__ == '__main__':
     os.system(f'cd /RSS/output/; python3 -m http.server {PORT}')
 
     while True:
+        logger.info('Generating new rss...')
         generate_rss()
+        logger.success('New rss generated.')
         sleep(60 * 5)
